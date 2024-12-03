14125a866884d5751b4232e15bfe7cc7e0af437e
Joseph Campbell <jsc940@gmail.com>
2024-12-03 10:48:22 -0700

    fix knapsack function

--- zero_one_knapsack_problem.py
+++ zero_one_knapsack_problem.py
@@ -15,15 +15,14 @@ def knapsack(capacity, weights, values):
     for i in range(1, n + 1):
         for w in range(capacity+1):
             # If the current items weight fits within the current knapsack capacity
-            if weights[i -1] <= w:
+            if weights[i - 1] <= w:
                 # The item fits, choose whichever is bigger, the previous rows value, or the current item + the value from the previous row that fits within the remaining capacity
-                dp[i][w] = max(dp[i-1][w], dp[i-1][capacity - weights[i-1]] + values[i-1])
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w - weights[i-1]] + values[i-1])
             # Skip adding this item because it does not fit
             else:
                 # Use the previous row's best
                 dp[i][w] = dp[i-1][w]
             print_2d_array(dp)
-                
     # Proceed with solving the problem (using dynamic programming or another approach)
     return dp[n][capacity]
 
